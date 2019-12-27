2065fab7e479d0077ef05ec51c7cf556500c5167
Fernando Alamillo <fernando.alamillo@creative-technologies.co.uk>
2019-12-27 16:07:52 +0000

    feat(eslint) updating config files

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-param-reassign, no-console */
+/* eslint-disable no-param-reassign, no-console, no-useless-concat */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -27,7 +27,7 @@ type Config = {
 export function register(config?: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(process.env.PUBLIC_URL as string, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -45,7 +45,7 @@ export function register(config?: Config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log('This web app is being served cache-first by a service worker. To learn more, visit https://bit.ly/CRA-PWA');
+          console.log('This web app is being served cache-first by a service ' + 'worker. To learn more, visit https://bit.ly/CRA-PWA');
         });
       } else {
         // Is not localhost. Just register service worker
@@ -70,7 +70,7 @@ function registerValidSW(swUrl: string, config?: Config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log('New content is available and will be used when all tabs for this page are closed. See https://bit.ly/CRA-PWA.');
+              console.log('New content is available and will be used when all ' + 'tabs for this page are closed. See https://bit.ly/CRA-PWA.');
 
               // Execute callback
               if (config && config.onUpdate) {
